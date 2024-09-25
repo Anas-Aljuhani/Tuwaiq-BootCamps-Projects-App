1706c30364b71c0b26c60bd037e23a9e68847519
Basel-75 <basel_1422@outlook.com>
2024-09-25 03:52:25 +0300

    remove unused imports

--- lib/screens/projectView/edit/componets/edit_base.dart
+++ lib/screens/projectView/edit/componets/edit_base.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 import 'package:tuwaiq_project/data_layer/language_layer.dart';
 import 'package:tuwaiq_project/helper/extinsion/size_config.dart';
 import 'package:tuwaiq_project/screens/projectView/edit/bloc/edit_bloc.dart';
--- lib/screens/projectView/edit/componets/edit_status.dart
+++ lib/screens/projectView/edit/componets/edit_status.dart
@@ -5,7 +5,6 @@ import 'package:tuwaiq_project/data_layer/language_layer.dart';
 import 'package:tuwaiq_project/helper/extinsion/size_config.dart';
 import 'package:tuwaiq_project/screens/projectView/edit/bloc/edit_bloc.dart';
 import 'package:tuwaiq_project/widget/button/custom_button.dart';
-import 'package:tuwaiq_project/widget/textformfeild/normal_text_form_feild.dart';
 
 class EditStatus extends StatelessWidget {
   const EditStatus({
